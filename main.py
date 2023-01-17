1c06ad4a4ed0b49beea03db0f972e7f4ec5f0bfb
NotFeesh <106921666+NotFeesh@users.noreply.github.com>
2023-01-17 08:17:39 -0800

    Finished basic code for holding matches

---
+++ main.py
@@ -0,0 +1,188 @@
+class PoolEd:
+  def __init__(self, pool1, pool2, pool3, pool4, pool5):
+    self.pools = [pool1, pool2, pool3, pool4, pool5]
+
+class PoolVar:
+  def __init__(self, map1, map2, map3, map4, map5):
+    self.maps = [map1, map2, map3, map4, map5]
+
+firstedition = PoolEd(PoolVar("Ruins", "Fortress", "Endstone", "Pillars", "Atlantis"), PoolVar("Glowstone", "StoneBurg", "StoneBurg 2.0", "Landscape", "Dust V"), PoolVar("Themepark", "Darkgrove", "Draze", "Tropical", "Slime"), PoolVar("Library", "Barnyard", "Subway", "Concrete", "Logs"), PoolVar("Icy", "Trainwreck", "Playground", "Hut", "Quartz"))
+
+secondedition = PoolEd(PoolVar("Subway", "Glowstone", "Library", "Slime", "Trainyard"), PoolVar("Ruins", "Endstone", "Tropical", "Blackstone", "Logs"), PoolVar("Quartz", "Trainwreck", "Playground", "Stoneburg", "Subway 2.0"), PoolVar("Icy", "Atlantis", "Hut", "Concrete", "Themepark"), PoolVar("Pillars", "Draze", "Barnyard", "Fortress", "Darkgrove"))
+
+editions = [firstedition, secondedition]
+
+print("Enter Player 1 Name:")
+player1 = input()
+
+print("Enter Player 2 Name:")
+player2 = input()
+
+minpool = 1
+maxpool = 2
+
+print("Enter Map Pool Edition Used: (" + str(minpool) + "-" + str(maxpool) + ")")
+ed = int(input())
+ed -= 1
+
+
+#p1 bans a pool; p2 bans a pool; p1 picks a pool to play first; p2 picks a pool to play second; 3rd pool automatically added;
+temp = [1, 2, 3, 4, 5]
+usedpools = []
+
+print(player1 + " bans a pool (1-5):")
+poolban1 = int(input())
+
+usedpools.append(poolban1)
+temp.remove(poolban1)
+
+print(player2 + " bans a pool (1-5)")
+poolban2 = poolban1
+while poolban2 in usedpools:
+  poolban2 = int(input())
+
+usedpools.append(poolban2)
+temp.remove(poolban2)
+
+print("Pools Banned: " + str(poolban1) + ", " + str(poolban2))
+
+print(player1 + " chooses to play pool (1-5) first:")
+poolpick1 = poolban1
+while poolpick1 in usedpools:
+  poolpick1 = int(input())
+
+usedpools.append(poolpick1)
+temp.remove(poolpick1)
+
+print(player2 + " chooses to play pool (1-5) second:")
+poolpick2 = poolban1
+while poolpick2 in usedpools:
+  poolpick2 = int(input())
+
+usedpools.append(poolpick2)
+temp.remove(poolpick2)
+
+pools = [poolpick1, poolpick2, temp[0]]
+
+print("The map pools will be played in the following order: " + str(pools[0]) + ", " + str(pools[1]) + ", and " + str(pools[2]))
+
+#P1-POOL: p2 bans a map; p1 bans a map; p2 picks a map to play first; p1 picks a map to play second; 3rd map automatically added;
+
+winner = 0
+
+pool_num = 1
+
+p1_poolscore = 0
+p2_poolscore = 0
+
+while p1_poolscore < 2 and p2_poolscore < 2:
+  print("PICK & BAN PHASE: POOL #" + str(pools[pool_num - 1]))
+
+  print(editions[ed].pools[pools[pool_num - 1] - 1].maps)
+
+  print(player2 + " bans a map: ")
+  mapban2 = 0
+  while mapban2 not in editions[ed].pools[pools[pool_num - 1] - 1].maps:
+    mapban2 = input()
+  editions[ed].pools[pools[pool_num - 1] - 1].maps.remove(mapban2)
+
+  print(player1 + " bans a map: ")
+  mapban1 = 0
+  while mapban1 not in editions[ed].pools[pools[pool_num - 1] - 1].maps:
+    mapban1 = input()
+  editions[ed].pools[pools[pool_num - 1] - 1].maps.remove(mapban1)
+
+  print("Maps Banned: " + mapban2 + ", " +  mapban1)
+
+  print(player2 + " chooses to play a map first: ")
+  mappick2 = 0
+  while mappick2 not in editions[ed].pools[pools[pool_num - 1] - 1].maps:
+    mappick2 = input()
+  editions[ed].pools[pools[pool_num - 1] - 1].maps.remove(mappick2)
+
+  print(player1 + " chooses to play a map second: ")
+  mappick1 = 0
+  while mappick1 not in editions[ed].pools[pools[pool_num - 1] - 1].maps:
+    mappick1 = input()
+  editions[ed].pools[pools[pool_num - 1] - 1].maps.remove(mappick1)
+
+  maps = [mappick2, mappick1, editions[ed].pools[pools[pool_num - 1] - 1].maps[0]]
+  print("Maps for Map Pool " + str(pools[pool_num - 1]) + " will be played in the following order: " + maps[0] + ", " + maps[1] + " & " + maps[2])
+
+  #Pool
+  print("Pool " + str(pool_num) + " : Match Score " + str(p1_poolscore) + " - " + str(p2_poolscore) + " : Map Pool " + str(pools[pool_num - 1]))
+  pool_winner = 0
+  #Map
+  map_num = 1
+
+  p1_mapscore = 0
+  p2_mapscore = 0
+
+  while p1_mapscore < 2 and p2_mapscore < 2:
+    #Map
+    print("Map " + str(map_num) + " : Map Score " + str(p1_mapscore) + " - " + str(p2_mapscore) + " : " + maps[map_num - 1])
+    mapwinner = 0
+    #Game
+    game_num = 1
+
+    p1_gamescore = 0
+    p2_gamescore = 0
+
+    while p1_gamescore < 2 and p2_gamescore < 2:
+      print("Game " + str(game_num) + " : Game Score " + str(p1_gamescore) + " - " + str(p2_gamescore))
+      print("Enter Game " + str(game_num) + " Winner")
+      gamewinner = input()
+      if gamewinner == player1:
+        p1_gamescore += 1
+      else:
+        p2_gamescore += 1
+      game_num += 1
+
+    if p1_gamescore > p2_gamescore:
+      mapwinner = player1
+    else:
+      mapwinner = player2
+
+    print(mapwinner + " wins " + str(p1_gamescore) + " - " + str(p2_gamescore) + " on " + maps[map_num - 1])
+    #Map
+    if mapwinner == player1:
+      p1_mapscore += 1
+    else:
+      p2_mapscore += 1
+    map_num += 1
+
+  if p1_mapscore > p2_mapscore:
+    poolwinner = player1
+  else:
+    poolwinner = player2
+
+  print(poolwinner + " wins " + str(p1_mapscore) + " - " + str(p2_mapscore) + " on map pool " + str(pools[pool_num - 1]))
+  if poolwinner == player1:
+    p1_poolscore += 1
+  else:
+    p2_poolscore += 1
+  pool_num += 1
+
+if p1_poolscore > p2_poolscore:
+  winner = player1
+else:
+  winner = player2
+
+print(winner + " wins!")
+
+
+
+  
+
+
+
+#MAP1: BO3; MAP2: BO3; MAP3: BO3;
+#P2-POOL: p1 bans a map; p2 bans a map; p1 picks a map to play first; p2 picks a map to play second; 3rd map automatically added;
+#MAP1: BO3; MAP2: BO3; MAP3: BO3;
+#Default-POOL: p1 bans a map; p2 bans a map; MAP1 picked randomly; MAP2 picked randomly; MAP3 picked randomly
+#MAP1: BO3; MAP2: BO3; MAP3: BO3;
+
+
+
+
+
